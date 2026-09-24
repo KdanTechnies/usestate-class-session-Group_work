c0561ffa3113243cbe508b9749057b1df98b2e3d
billcrx <billchijindu@gmail.com>
2026-09-24 13:28:45 +0100

    routing update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
+import './styles/global.css';
 // Import components
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
